75af3ce82f7f0e2e0d340870dfba8a3894a9ba4f
Caleb Silva <106850025+CalebSilvaBr@users.noreply.github.com>
2022-10-14 16:39:13 -0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,7 +94,7 @@ def registration_request(request):
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-    return render(request, 'djangoapp/index.html', context)
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
